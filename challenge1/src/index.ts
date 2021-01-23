34efdf34e68b1c982ef102de37765ffc8392b2a7
matteo <matteo@matteos-MacBook-Pro.local>
2021-01-23 12:46:04 -0800

    first commit

---
+++ challenge1/src/index.ts
@@ -0,0 +1,72 @@
+// INSTRUCTIONS:
+// Replace unknown type with the correct type
+let numberOfDishes: number = 50;
+
+let favoriteSnack: string = "cake";
+
+const ingredients: string[] = [
+  "butter",
+  "flour",
+  "baking powder",
+  "sugar",
+  "eggs",
+  "vanilla",
+  "salt",
+];
+
+// The object is missing a value. Fill in the missing value with drinks of your choice.
+const menu: {
+  courses: number;
+  veganOption: boolean;
+  drinkChoices: string[];
+} = {
+  courses: 5,
+  veganOption: true,
+  drinkChoices: [ 'Bourbon', 'Gin' ]
+};
+
+interface fruit {
+    name: string,
+    color: string,
+    sweetness: number
+}
+
+const strawberry: fruit = {
+  name: "Strawberry",
+  color: "red",
+  sweetness: 80,
+};
+
+// Correctly annotate this function parameter
+function eatFruit(fruit: fruit): void {
+  if (fruit.name === "Strawberry") {
+    console.log("I love Strawberry!");
+  }
+  console.log("Fruit is very tasty.");
+}
+
+eatFruit(strawberry);
+
+// Correctly annotate the fruitList and transformFunction parameters
+function mapFruit(fruitList: string[], transformFunction) {
+  return fruitList.map(transformFunction);
+}
+
+const fruitList = ["Apple", "Banana", "Orange"];
+
+// Annotate the fruitName function parameter
+function bakeFruit(fruitName: string): string {
+  return `Baked ${fruitName}`;
+}
+
+const bakedFruit = mapFruit(fruitList, bakeFruit);
+
+/*
+// greetMe(name : string, callbackFn: (msg: string) => void )
+
+  function greetMe(name, callback) {
+    callback("Hello there" + name)
+  }
+  greetMe("John", (msg) => { console.log(msg) })
+
+*/
---
+++ challenge2/src/index.ts
@@ -0,0 +1,53 @@
+// Create an interface that represents the data used in the function below it.
+// Type the function parameter with your interface.
+
+interface Fruit {}
+
+function fruitMessage(fruit) {
+  let review: string = `This ${fruit.name} has a nice ${fruit.color} color.`;
+  if (fruit.sweetness < 50) {
+    review += " It should be a little sweeter.";
+  } else {
+    review += " When I tasted it, it was just right.";
+  }
+
+  review += ` I would give it ${fruit.stars} stars.`;
+
+  return review;
+}
+
+console.log(
+  fruitMessage({ name: "Apple", color: "red", sweetness: 80, stars: 4.5 })
+);
+
+// Create an interface for an Apple by extending the Fruit interface you already made
+// Add a string property to represent the 'kind' of apple. (Ex - Fuji Apple)
+
+interface Apple {}
+
+// Final Problem:
+/*
+
+ Create an interface called Class that contains:
+ - An interface representing a Teacher
+ - An interface representing a list of Student
+
+ Teacher and Student contain a fullName, age, and bloodType. 
+ The only difference is: Teacher has facultyId and student has studentId.
+ Use interface inheritance through "extends" to share the common properties
+ through a Person interface. 
+ 
+ Class should look like this:
+
+ interface Class {
+   instructor: Teacher;
+   students: Student[]
+ }
+
+ Create a variable called oopClass which contains a fake object with data you create
+ that correctly follows the Class interface. 
+
+ let oopClass: Class = {
+   // fill this out
+ }
+*/
